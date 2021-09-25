2ef5e56911752cd7a3d6d6e3a31c82d72d17d382
elonymac <elonymac@gmail.com>
2021-09-26 00:37:18 +0200

    Refaktoryzacja, dodany ruch z ogranicznkiem

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,6 @@ class AlienInvasion:
     def __init__(self):
         """Inicjalizacja gry i utworzenie jej zasobow."""
         pygame.init()
-
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
@@ -25,6 +24,7 @@ class AlienInvasion:
         """Rozpoczecie petli glownej gry."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Odswiezanie ekranu w trakcie kazdej iteracji petli.
 
@@ -33,6 +33,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Reakcja na nacisniecie klawisza."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Reakcja na zwolnienie klawisza."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _update_screen(self):
         """Uaktualnienie obrazow na ekranie i przejscie do nowego ekranu"""
--- ship.py
+++ ship.py
@@ -5,9 +5,10 @@ class Ship:
     """Klasa przeznaczona do zarzadzania statkiem kosmicznym"""
 
     def __init__(self, ai_game):
-        "Inicjalizacja statku kosmicznego i jego polozenie poczatkowe"
+        """Inicjalizacja statku kosmicznego i jego polozenie poczatkowe"""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Wczytanie obrazu statku kosmicznego i pobranie jego prostokata.
@@ -18,6 +19,26 @@ class Ship:
         # Tutaj umiejscawiamy "rect" obrazka w miejscu screen_rect
         # z obrazu przyslanego z funkcji ai_game tam wyzej /\
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Polozenie poziome statku jest przechowywane w potaci liczby zmiennoprzecinkowej.
+        self.x = float(self.rect.x)
+
+        # Opcje wskazujace na poruszanie sie statku.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """
+        Uaktualnienei polzenia statku na podstawie opcji wskazujacej
+        na jego ruch
+        """
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Uaktualnienie obiektu rect na podstawie wartosci self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Wyswietlanie statku kosmicznego w jego aktualnym polozeniu"""
