fbb18ebb4c3c82f6a20665c5d7f03b3756fcd858
elonymac <elonymac@gmail.com>
2021-10-03 11:15:07 +0200

    dodano pociski

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -19,12 +20,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Rozpoczecie petli glownej gry."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
             # Odswiezanie ekranu w trakcie kazdej iteracji petli.
 
@@ -44,6 +47,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Reakcja na zwolnienie klawisza."""
@@ -54,10 +61,33 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
 
+    def _fire_bullet(self):
+        """Utworzenie nowego pocisku i dodanie go do grupy pociskow."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Uaktualnienie polozenia pociskow i usun iecie tych niewidocznych na ekranie"""
+        # Uaktualnieni polozenia pociskow.
+        self.bullets.update()
+
+        # Usuniecie pociskow, ktore znajduja sie poza ekranem.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
+
+        # Usuniecie pociskow, ktore znajduja sie poza ekranem.
+
+
     def _update_screen(self):
         """Uaktualnienie obrazow na ekranie i przejscie do nowego ekranu"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
 
         # Wyswietlanie ostatnio zmodyfikowanego ekranu.
         pygame.display.flip()
